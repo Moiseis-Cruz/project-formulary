e546b4534918113028ef453c18cb5874dcbbcf01
Moiseis-Cruz <moizzito@gmail.com>
2023-02-18 12:43:42 -0300

    Declarei as variáveis para trabalhar.

---
+++ src/js/index.js
@@ -0,0 +1,7 @@
+let userName = document.getElementById("name")
+
+let email = document.getElementById("email")
+
+let phone = document.getElementById("phone");
+
+let mensagem = document.getElementById("mensagem")
