d2ac608b4cccc5e0272c68159f9958c81f20067c
Moiseis-Cruz <moizzito@gmail.com>
2023-02-18 20:05:35 -0300

    Fazendo os testes...

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,18 @@
 let userName = document.getElementById("name")
-
 let email = document.getElementById("email")
-
-let phone = document.getElementById("phone");
-
+let phone = document.getElementById("phone")
 let mensagem = document.getElementById("mensagem")
+
+let formulary = document.querySelector("form[id='formulary']")
+
+formulary.addEventListener("submit", function () {
+
+    let itemInvalid = document.querySelectorAll(".campo")
+    let itemEnabled = document.querySelector(".campo")
+
+    if(userName === ""){
+        itemInvalid.classList.add("invalid")
+    }else{
+        itemEnabled.classList.add("enabled")
+    }
+})
