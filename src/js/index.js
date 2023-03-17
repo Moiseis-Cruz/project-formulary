69a3d767fd545d20cf743f88a873edeae32807cd
Moiseis-Cruz <moizzito@gmail.com>
2023-03-17 20:32:41 -0300

    Comecei com variáveis e funções.

--- src/js/index.js
+++ src/js/index.js
@@ -8,10 +8,34 @@ btnValidation.addEventListener("click", event => {
     const phone = document.getElementById("phone");
     const message = document.getElementById("message")
 
-    const nameValue = name.value
-    const emailValue = email.value
-    const phoneValue = phone.value
-    const messageValue = message.value
+    const nameValue = name.value.trim()
+    const emailValue = email.value.trim()
+    const phoneValue = phone.value.trim()
+    const messageValue = message.value.trim()
 
-    
+    if(nameValue === ""){
+        errorValidation(name)
+    }else{
+        enabledValidation(name)
+    }
+
+    if(emailValue === ""){
+        errorValidation(email)
+    }else{
+        enabledValidation(email)
+    }
 })
+
+function errorValidation() {
+    const formControl = document.querySelector(".form-control")
+    // const smallInvalid = document.querySelector("warning")
+
+    formControl.classList.add("invalid")
+    console.log(formControl);
+}
+
+function enabledValidation() {
+    const formControl = document.querySelector(".form-control")
+
+    formControl.classList.add("enabled")
+}
