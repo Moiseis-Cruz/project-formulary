9bb813ec693746c66c17040a6b72f1937ab20ce9
Moiseis-Cruz <moizzito@gmail.com>
2023-04-09 17:05:46 -0300

    Concluindo trabalho... Falra muito pouco.

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,17 @@
 const btnValidation = document.getElementById("btn-validation")
 
+const divsValidation = document.querySelectorAll(".form-control")
+
+const input = document.querySelectorAll("#name, #email, #phone, #message")
+
 btnValidation.addEventListener("click", event => {
     event.preventDefault()
 
-
+    input.forEach(function(input, i) {
+        if(input.value === ""){
+            divsValidation[i].classList.add("invalid")
+        }else{
+            divsValidation[i].classList.add("enabled")
+        }
+    })
 })
