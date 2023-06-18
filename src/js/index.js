090d15b0beb73ba4e05fddc95a0fc790c7574a0d
Moiseis-Cruz <moizzito@gmail.com>
2023-06-18 08:47:53 -0300

    Mudei o evento de click para submit.

--- src/js/index.js
+++ src/js/index.js
@@ -1,19 +1,19 @@
-const btnValidation = document.getElementById("btn-validation")
+const formulary = document.getElementById("formulary")
 
 const divsValidation = document.querySelectorAll(".form-control")
 
 const input = document.querySelectorAll("#name, #email, #phone, #message")
 
-btnValidation.addEventListener("click", event => {
-    event.preventDefault()
+formulary.addEventListener("submit", (e) => {
+    e.preventDefault()
 
-    input.forEach(function(input, i) {
+    input.forEach((input, i) => {
         if(input.value === ""){
             divsValidation[i].classList.add("invalid")
             divsValidation[i].classList.remove("enabled")
         }else{
-            divsValidation[i].classList.add("enabled")
             divsValidation[i].classList.remove("invalid")
+            divsValidation[i].classList.add("enabled")
         }
     })
 })
