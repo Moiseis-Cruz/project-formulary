9aa9c3341da13ec02a6b56fb8bbb2b690955d971
moizzito@gmail.com <moizzito@gmail.com>
2023-08-08 12:14:23 -0300

    Add evento de change.

--- src/js/index.js
+++ src/js/index.js
@@ -4,6 +4,20 @@ const divsValidation = document.querySelectorAll(".form-control")
 
 const input = document.querySelectorAll("#name, #email, #phone, #message")
 
+formulary.addEventListener("change", (e) => {
+    e.preventDefault()
+
+    input.forEach((input, i) => {
+        if(input.value === ""){
+            divsValidation[i].classList.add("invalid")
+            divsValidation[i].classList.remove("enabled")
+        }else{
+            divsValidation[i].classList.remove("invalid")
+            divsValidation[i].classList.add("enabled")
+        }
+    })
+})
+
 formulary.addEventListener("submit", (e) => {
     e.preventDefault()
 
