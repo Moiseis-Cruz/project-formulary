7dcb169cabc5c52b74f401ec5d56c1518402d7ca
Moiseis Cruz <moizzito@gmail.com>
2024-10-17 19:02:50 -0300

    Organizei o código.

--- src/js/index.js
+++ src/js/index.js
@@ -1,33 +1,33 @@
-const formulary = document.getElementById("formulary")
+const formulary = document.getElementById("formulary");
 
-const divsValidation = document.querySelectorAll(".form-control")
+const divsValidation = document.querySelectorAll(".form-control");
 
-const input = document.querySelectorAll("#name, #email, #phone, #message")
+const input = document.querySelectorAll("#name, #email, #phone, #message");
 
 formulary.addEventListener("change", (e) => {
-    e.preventDefault()
+    e.preventDefault();
 
     input.forEach((input, i) => {
         if(input.value === ""){
-            divsValidation[i].classList.add("invalid")
-            divsValidation[i].classList.remove("enabled")
+            divsValidation[i].classList.add("invalid");
+            divsValidation[i].classList.remove("enabled");
         }else{
-            divsValidation[i].classList.remove("invalid")
-            divsValidation[i].classList.add("enabled")
-        }
-    })
-})
+            divsValidation[i].classList.remove("invalid");
+            divsValidation[i].classList.add("enabled");
+        };
+    });
+});
 
 formulary.addEventListener("submit", (e) => {
-    e.preventDefault()
+    e.preventDefault();
 
     input.forEach((input, i) => {
         if(input.value === ""){
-            divsValidation[i].classList.add("invalid")
-            divsValidation[i].classList.remove("enabled")
+            divsValidation[i].classList.add("invalid");
+            divsValidation[i].classList.remove("enabled");
         }else{
-            divsValidation[i].classList.remove("invalid")
-            divsValidation[i].classList.add("enabled")
-        }
-    })
-})
+            divsValidation[i].classList.remove("invalid");
+            divsValidation[i].classList.add("enabled");
+        };
+    });
+});
